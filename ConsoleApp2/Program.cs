bb2cf63303c78042296dbff6fe867dc7b3342b76
PappG <PappG@310-14>
2025-11-24 09:42:14 +0100

    asd

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,74 +1,85 @@
-﻿using System;
-using System.ComponentModel;
-
-namespace MyApp
+﻿namespace Nevekszurese
 {
     internal class Program
     {
-        
         static void Main(string[] args)
         {
-
-            List<string> lista = new List<string>();
-            lista.Add("Deak Csaba");
-            lista.Add("Kiss István József");
-            lista.Add("Nagy István Elemér");
-            lista.Add("Farkas1 Aladár");
-            lista.Add("Pető@Ilona.hu");
-            lista.Add("Pisti");
-            lista.Add("12Elemér");
-            lista.Add("SzekeresKata");
-
-
-            while (true) 
+            List<string> list = new List<string>();
+            list.Add("Koleszár Emil");
+            list.Add("Kiss István József");
+            list.Add("rácrz László");
+            list.Add("Farkas1 Aladár");
+            list.Add("Pető@Ilona.hu");
+            list.Add("Pisti");
+            list.Add("12Elemér");
+            list.Add("SzekeresKata");
+            while (true)
             {
                 Console.Clear();
-                Console.WriteLine("0 - Kilépés");
-                Console.WriteLine("1 - Összes név listázása");
-                Console.WriteLine("2 - Csak betűt tartalamzaó nevek listázása");
-                Console.WriteLine("3 - Nagybetűvel kezdődő nevek listázása");
-                Console.WriteLine("4 - Rövid nevek listázása");
-                Console.WriteLine("5 - Nagybetűvel kezdődő nevek ahol a többi kicsi"+"és nem tartalmaz számot és speciális jelet");
+                Console.Write("0 - Kilépés");
+                Console.Write("1 - Összes név listázása");
+                Console.Write("2 - Csak betűket tartalmazó nevek listázása");
+                Console.Write("3 - Nagybetűvel kezdődő nevek listázása");
+                Console.Write("4 - Rövid nevek listázása");
+                Console.Write("5 - Nagybetűvel kezdődő nevek ahol a többi kicsi" + "és nem tartalmaz számot és speciális jelet");
                 string valasztas = Console.ReadLine();
-                switch (valasztas) 
+                switch (valasztas)
                 {
-                    case "0":return;//kilepes
+                    case "0": return; //Kilépés
                     case "1":
                         Console.Clear();
                         int sorszam = 0;
-                        foreach(string nev in lista) Console.WriteLine($"{sorszam++}. { nev}");
+                        foreach (string nev in list)
+                        {
+                            Console.WriteLine($"{sorszam++}. {nev}");
+                        }
                         break;
-                        
-
-
                     case "2":
                         Console.Clear();
                         sorszam = 0;
-                        foreach (string nev in lista)
+                        foreach (string nev in list)
                         {
                             bool jo = true;
                             foreach (char betu in nev)
                             {
-                                if (!Char.IsLetter(betu) && betu == ' ')
-                                    jo = true;
+                                if (!Char.IsLetter(betu) && betu != ' ')
+                                    jo = false;
                             }
                             if (jo)
-                                Console.WriteLine($"{sorszam++}. {nev}");
+                                Console.WriteLine($"{sorszam++}: {nev}");
                         }
-                        
-                        
-                        
-                        
-                        
-                        break;
 
-                    case "3": break;
-                    case "4": break;
-                    case "5": break;
-                    default: Console.WriteLine("Rossz parancs");break;
+                        break;
+                    case "3": Console.WriteLine("3"); break;
+                    case "4": Console.WriteLine("4"); break;
+                    case "5":
+                        Console.Clear();
+                        sorszam = 0;
+                        foreach (string nev in list)
+                        {
+                            bool jo = true;
+                            foreach (char betu in nev)
+                            {
+                                if (!Char.IsLetter(betu) && betu != ' ')
+                                    jo = false;
+                            }
+                            String[] nevdarabok = nev.Split(' ');
+                            foreach (var nevdarab in nevdarabok)
+                            {
+                                if (!Char.IsUpper(nevdarab[0]))
+                                    jo = false;
+                                if (nevdarab.Substring(1) != nevdarab.Substring(1).ToLower())
+                                    jo = false;
+                            }
+                            if (jo)
+                                Console.WriteLine($"{sorszam++}: {nev}");
+                        }
+
+                        break;
+                    default: Console.WriteLine("Érvénytelen parancs!"); break;
                 }
                 Console.ReadKey();
-                
+
             }
         }
     }
